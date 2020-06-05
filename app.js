e3bfff22c7c05803535321b7e1ba884c9456ff60
Oladipupo Oluwatobi <oladipupooluwatobi@Oladipupos-MacBook-Pro.local>
2020-06-05 08:23:39 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,69 @@
+const express  = require('express');
+const request  = require('request');
+const bodyParser = require('body-parser');
+const https = require('https');
+
+
+const app = express();
+app.use(express.static('new_public'));
+app.use(bodyParser.urlencoded({extended:true}))
+
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/signup.html")
+});
+
+app.post("/", function(req,res){
+   
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    //console.log(firstName,lastName,email);
+   
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+   
+    };
+
+    const jsonData = JSON.stringify(data);
+    
+    const url = "https://us10.api.mailchimp.com/3.0/lists/669649698f";
+
+    const Options = {
+        method: "POST",
+        auth: "oluwatobi:858b9b37754d69b44541cac1232180a7-us10",
+    }
+
+    const request = https.request(url, Options, function(response) {
+
+        if (response.statusCode == 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+       response.on("data", function(data)
+       {
+        console.log(JSON.parse(data));
+       }) 
+    })
+    request.write(jsonData);
+    request.end();
+});
+
+app.post("/failure", function(req, res){
+    res.redirect("/")
+})
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server is running on port 3000");
+});
+
